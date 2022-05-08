ca06df791616a86b75fe7e9f9bf68f213db9dbd8
Murat Topallar <topallarm@outlook.com>
2022-05-09 00:20:28 +0300

    Migration oluşturulup PackageManagerConsole ile veritabanı oluşturuldu, api aracılığı ile testler yapıldı. IOC olarak WebAPI'nin kendi altyapısı kullanıldı.

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Business.Abstract;
+using Entities.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers
@@ -7,5 +8,30 @@ namespace WebAPI.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            return Ok(_productService.GetById(id));
+        }
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            return Ok(_productService.GetAll());
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            _productService.Add(product);
+            return Ok("Başarılı");
+        }
+
     }
 }
